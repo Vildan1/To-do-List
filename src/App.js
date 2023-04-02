0b3dc69cb0f37dede85dc7c4aa261e54af89beb8
vildan1 <vildanakbulut98@gmail.com>
2023-04-02 10:12:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+import './App.css';
+import Components from './components/component';
+
+const  App =()=> {
+  return (
+    <div className="App">
+      <Components/>
+    </div>
+  );
+}
+
+export default App;
